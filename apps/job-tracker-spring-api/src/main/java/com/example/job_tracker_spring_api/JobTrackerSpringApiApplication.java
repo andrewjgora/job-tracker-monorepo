284f5ecbaa8fcc46651e43726e145230755d696a
agora94 <gora.andrew@gmail.com>
2024-07-30 16:19:00 -0400

    Init spring boot api

---
+++ apps/job-tracker-spring-api/src/main/java/com/example/job_tracker_spring_api/JobTrackerSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.job_tracker_spring_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackerSpringApiApplication.class, args);
+	}
+
+}
---
+++ apps/job-tracker-spring-api/src/test/java/com/example/job_tracker_spring_api/JobTrackerSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job_tracker_spring_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
